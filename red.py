928e71524858b80aee11cab5201d3031cceb1239
wee252 <34206759+wee252@users.noreply.github.com>
2018-08-11 00:29:34 +0700

    Update red.py

--- red.py
+++ red.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from linepy import *
-from akad.ttypes import *
+from Rin2.linepy import *
+from Rin2.akad.ttypes import *
 from multiprocessing import Pool, Process
 from datetime import datetime
 from time import sleep
 from bs4 import BeautifulSoup
 from humanfriendly import format_timespan, format_size, format_number, format_length
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,antolib,subprocess,unicodedata,GACSender
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,subprocess,unicodedata,GACSender
 from gtts import gTTS
 from googletrans import Translator
 #==============================================================================#
